52184a22c572ba5bf3498561b65106861729fcf8
George Leontiev <folone@gmail.com>
2013-11-26 09:54:20 +0100

    Implementation with typelevel hlist.

--- src/main/scala/info.folone/hanoi.scala
+++ src/main/scala/info.folone/hanoi.scala
@@ -3,6 +3,7 @@ package info.folone
 import shapeless._
 import nat._
 import ops.nat._
+import ops.hlist._
 
 
 /**
@@ -22,77 +23,46 @@ import ops.nat._
   *   move(M, C, R, L).
   */
 object Hanoi {
-
-  // Show typeclass
-  trait Show[T] {
-    def show: String
-  }
-
-  // Color utilities
-  def wrap(str: String, color: String) = color ++ str ++ "\u001B[0m"
-  def green(str: String)  = wrap(str, "\u001B[32m")
-  def yellow(str: String) = wrap(str, "\u001B[33m")
-  def blue(str: String)   = wrap(str, "\u001B[34m")
-
-
   sealed abstract trait      Name
   case object Left   extends Name
   case object Right  extends Name
   case object Center extends Name
 
-  implicit val leftShow:   Show[Left.type]   = new Show[Left.type] {
-    def show = yellow("Left")
-  }
-  implicit val rightShow:  Show[Right.type]  = new Show[Right.type] {
-    def show = green("Right")
-  }
-  implicit val centerShow: Show[Center.type] = new Show[Center.type] {
-    def show = blue("Center")
-  }
-
-  trait Move[N <: Nat, L <: Name, R <: Name, C <: Name] {
-    val acc : String
-  }
+  trait Move[N <: Nat, L <: Name, R <: Name, C <: Name] { type Out <: HList }
 
   object Move {
-    // Direct translation of prolog code.
+    type Aux[N <: Nat, L <: Name, R <: Name, C <: Name, Out0 <: HList] =
+      Move[N, L, R, C] { type Out = Out0 }
+
     implicit def move0[L <: Name,
                        R <: Name,
-                       C <: Name]
-      (implicit ev0 : Show[L],
-                ev1 : Show[R]):
-        Move[_1, L, R, C] =
+                       C <: Name]:
+        Aux[_1, L, R, C, (L, R) :: HNil] =
           new Move[_1, L, R, C] {
-            val acc = "Move top disk from\t" + ev0.show + "\tto\t" + ev1.show
+            type Out = (L, R) :: HNil
           }
     implicit def move1[N <: Nat,
                        M <: Nat,
                        L <: Name,
                        R <: Name,
-                       C <: Name]
+                       C <: Name,
+                       Out0 <: HList,
+                       Out1 <: HList,
+                       Out2 <: HList,
+                       Out3 <: HList,
+                       Out4 <: HList]
       (implicit ev0 : LT[_1, N],
                 ev1 : Diff.Aux[N, _1, M],
-                ev2 : Move[M, L, C, R],
-                ev3 : Move[_1, L, R, C],
-                ev4 : Move[M, C, R, L],
-                ev5 : Show[Move[M, L, C, R]],
-                ev6 : Show[Move[_1, L, R, C]],
-                ev7 : Show[Move[M, C, R, L]]):
-        Move[N, L, R, C] =
+                ev2 : Aux[M, L, C, R, Out0],
+                ev3 : Aux[_1, L, R, C, Out1],
+                ev4 : Aux[M, C, R, L, Out2],
+                ev5 : Prepend.Aux[Out1, Out2, Out3],
+                ev6 : Prepend.Aux[Out0, Out3, Out4]):
+        Aux[N, L, R, C, Out4] =
           new Move[N, L, R, C] {
-            val acc = ev5.show ++ "\n" ++ ev6.show ++ "\n" ++ ev7.show
+            type Out = Out4
           }
-
-    // Show instance to actually see the result.
-    implicit def moveShow[N <: Nat,
-                          L <: Name,
-                          R <: Name,
-                          C <: Name]
-      (implicit ev: Move[N, L, R, C]) =
-        new Show[Move[N, L, R, C]] {
-          def show = ev.acc
-        }
   }
-
-  // implicitly[Show[Move[_3, Left.type, Right.type, Center.type]]].show
+  // type Res = (Left.type, Center.type) :: (Left.type, Right.type) :: (Center.type, Right.type) :: HNil
+  // implicitly[Move.Aux[_2, Left.type, Right.type, Center.type, Res]]
 }
