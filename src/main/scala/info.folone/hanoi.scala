2cb9586fdcea0a732865f4c72d3fa9fdc08e1fce
George Leontiev <folone@gmail.com>
2013-11-26 09:38:38 +0100

    Formatting for initial solution.

--- src/main/scala/info.folone/hanoi.scala
+++ src/main/scala/info.folone/hanoi.scala
@@ -8,18 +8,18 @@ import ops.nat._
 /**
   * Prolog code:
   *
-  * move(1,X,Y,_) :-  
-  *   write('Move top disk from '), 
-  *   write(X), 
-  *   write(' to '), 
-  *   write(Y), 
-  *   nl. 
-  * move(N,X,Y,Z) :- 
-  *   N>1, 
-  *   M is N-1, 
-  *   move(M,X,Z,Y), 
-  *   move(1,X,Y,_), 
-  *   move(M,Z,Y,X).  
+  * move(1, L, R, C) :-
+  *   write('Move top disk from '),
+  *   write(L),
+  *   write(' to '),
+  *   write(R),
+  *   nl.
+  * move(N, L, R, C) :-
+  *   N > 1,
+  *   M is N - 1,
+  *   move(M, L, C, R),
+  *   move(1, L, R, C),
+  *   move(M, C, R, L).
   */
 object Hanoi {
   sealed abstract trait      Name
@@ -31,11 +31,22 @@ object Hanoi {
 
   object Move {
     // Direct translation of prolog code.
-    implicit def move0[L <: Name, R <: Name, C <: Name]: Move[_1, L, R, C] =
-        new Move[_1, L, R, C] {}
-    implicit def move1[N <: Nat, M <: Nat, L <: Name, R <: Name, C <: Name]
-      (implicit ev0: LT[_1, N], ev1: Diff.Aux[N, _1, M], ev2: Move[M, L, C, R],
-        ev3: Move[_1, L, R, C], ev4: Move[M, C, R, L]): Move[N, L, R, C] =
+    implicit def move0[L <: Name,
+                       R <: Name,
+                       C <: Name]:
+        Move[_1, L, R, C] =
+          new Move[_1, L, R, C] {}
+    implicit def move1[N <: Nat,
+                       M <: Nat,
+                       L <: Name,
+                       R <: Name,
+                       C <: Name]
+      (implicit ev0 : LT[_1, N],
+                ev1 : Diff.Aux[N, _1, M],
+                ev2 : Move[M, L, C, R],
+                ev3 : Move[_1, L, R, C],
+                ev4 : Move[M, C, R, L]):
+        Move[N, L, R, C] =
           new Move[N, L, R, C] {}
   }
 
